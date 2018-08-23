fde0331a9d9455ef25fb9fb5f5c2f01371f22108
Caresa Huddleston <caresa.huddleston@gmail.com>
2018-08-23 11:45:52 -0500

    moved youTube video to top of js-results so the wiki would expand downward

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 //api endpoint
 const BASE_URL = "https://tastedive.com/api/similar?"
 
@@ -32,7 +31,7 @@ function watchSubmit() {
             $('.js-warn').html(`Please enter a valid query`);
         } else {
             getData(typeVal, queryVal, function (data) {
-                if (data.Similar.Results.length > 0){
+                if (data.Similar.Results.length > 0) {
                     $('.js-results').html("");
                     for (var i in data.Similar.Results) {
                         $('js-results').append(data.Similar.Results[i].Name);
@@ -48,7 +47,7 @@ function watchSubmit() {
                         collapsedHeight: 62,
                         //overflow: hidden;
                     });
-                }else{
+                } else {
                     $('.js-warn').html(`Please enter a valid query`);
                 }
             });
@@ -61,17 +60,17 @@ function getResultMarkup(data) {
     let output =
         `
     <div class="contain">
+        <div class="js-video">
+            <a class="js-result-name" href= "https://www.youtube.com/embed/${data.yID}" target="_blank"
+            data-featherlight="iframe" data-featherlight-iframe-frameborder="0"
+            data-featherlight-iframe-allow="autoplay; encrypted-media" data-featherlight-iframe-allowfullscreen="true"
+            data-featherlight-iframe-style="display:block;border:none;height:85vh;width:85vw;">
+            <iframe width="100%" height="150" src="https://www.youtube.com/embed/${data.yID}" frameborder="0" allow="autoplay;      encrypted-media" allowfullscreen></iframe>
+            </a>
+        </div>
         <p class="js-name clearfix">${data.Name}</p>
         <div class="js-info">
             <span class="readmore-contain">${data.wTeaser}</span>
-        </div>
-        <div class="js-video">
-            <a class="js-result-name" href= "https://www.youtube.com/embed/${data.yID}" target="_blank"
-                data-featherlight="iframe" data-featherlight-iframe-frameborder="0"
-                data-featherlight-iframe-allow="autoplay; encrypted-media" data-featherlight-iframe-allowfullscreen="true"
-                data-featherlight-iframe-style="display:block;border:none;height:85vh;width:85vw;">
-                <iframe width="100%" height="150" src="https://www.youtube.com/embed/${data.yID}" frameborder="0" allow="autoplay;      encrypted-media" allowfullscreen></iframe>
-            </a>
         </div>
     </div>
     `;
